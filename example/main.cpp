c5fcab6df95fff0793152f07016bc32e94ce671a
Boris <boris.bordeaux@gmail.com>
2025-09-28 14:12:27 +0200

    Fixed window name

--- example/main.cpp
+++ example/main.cpp
@@ -251,7 +251,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
 
     // Create window with graphics context
     float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor()); // Valid on GLFW 3.3+ only
-    GLFWwindow* window = glfwCreateWindow((int) (1280 * main_scale), (int) (800 * main_scale), "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow((int) (1280 * main_scale), (int) (800 * main_scale), "MassSpringSystem example", nullptr, nullptr);
     if (window == nullptr)
         return 1;
     glfwMakeContextCurrent(window);
